55c41fc83e875922989a139029e29681fe14b25e
ZaBot-MD <94165730+ZaBot-MD@users.noreply.github.com>
2022-03-20 06:56:10 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,10 @@
 let handler =  m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [085713964963]
-│ • Telkomsel [081319944687]
+│ • Telkomsel [081227144597]
 ╰────
 
 ╭─「 Donasi • Non Pulsa 」
-│ • https://saweria.co/BochilGaming
-│ • Gopay [085713964963]
+│ • Gopay [081227144597]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
